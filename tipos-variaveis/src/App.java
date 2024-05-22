fa61c28c36f5e0d8793f6e3828e2b54b26e79169
dudete <duda06736@gmail.com>
2024-05-21 21:30:01 -0300

    semana de aprendizado com java

--- tipos-variaveis/src/App.java
+++ tipos-variaveis/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, Worldd!");
+        System.out.println("Eu sou linda de maissss");
     }
 }
